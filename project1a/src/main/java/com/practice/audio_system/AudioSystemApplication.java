207a3b11c316e218c2d497e60840b845cc42d69f
johnmramirez <johnmichaelramirez7@gmail.com>
2023-12-19 19:16:10 -0600

    Initial commit

---
+++ project1a/src/main/java/com/practice/audio_system/AudioSystemApplication.java
@@ -0,0 +1,13 @@
+package com.practice.audio_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioSystemApplication.class, args);
+	}
+
+}
---
+++ project1a/src/test/java/com/practice/audio_system/AudioSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.audio_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
